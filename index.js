b9f5071804e103d699c9d2fd9548165fc245340a
Matthew Beale <matt.beale@madhatted.com>
2017-07-26 23:25:01 -0400

    Fix up Ember compat

--- index.js
+++ index.js
@@ -7,24 +7,29 @@ var CSSInjector = require('./lib/css_injector');
 var path = require('path');
 var fs = require('fs');
 
-var PLACEHOLDER = new RegExp('<link rel="stylesheet" href=".*app.*\.css">');
-
 function CSSReader(inputNodes, options) {
   options = options || {};
   Plugin.call(this, inputNodes, {
     annotation: options.annotation
   });
   this.options = options;
+  this._cssFileRegExp = new RegExp(`${this.options.appName}.*\.css`);
+  this._stylesheetLinkRegExp = new RegExp(`<link rel="stylesheet" href=".*${this.options.appName}.*\.css">`);
 }
 CSSReader.prototype = Object.create(Plugin.prototype);
 CSSReader.prototype.constructor = CSSReader;
 
 CSSReader.prototype.build = function() {
-  let cssPath = fs.readdirSync(this.inputPaths[0]).find(file => file.match(/app.*\.css/));
+  let cssPath = fs.readdirSync(this.inputPaths[0]).find(file => file.match(this._cssFileRegExp));
+  /* Ember files are in a subdirectory */
+  if (!cssPath) {
+    let assetCssPath = fs.readdirSync(path.join(this.inputPaths[0], 'assets')).find(file => file.match(this._cssFileRegExp));
+    cssPath = `assets/${assetCssPath}`;
+  }
   var injector = new CSSInjector({
     template: `${fs.readFileSync(path.join(this.inputPaths[0], 'index.html'))}`,
     css: `${fs.readFileSync(path.join(this.inputPaths[0], cssPath))}`,
-    placeholder: PLACEHOLDER
+    placeholder: this._stylesheetLinkRegExp
   });
 
   injector.write(path.join(this.outputPath, 'index.html'));
@@ -39,7 +44,22 @@ module.exports = {
 
   postprocessTree(type, tree) {
     if (type === 'all') {
-      return new MergeTrees([tree, new CSSReader([tree])], {
+      let cssReaderTree = new CSSReader([tree], {
+        appName: (
+          (
+            /* Basically detect Ember */
+            (this.app.options.outputPaths && this.app.options.outputPaths.app.css.app) ?
+            /* This isn't actually right, should use the output path
+             * above
+             */
+            this.app.name :
+            'app'
+          ) ||
+          /* Fallback to app for glimmer */
+          'app'
+        )
+      });
+      return new MergeTrees([tree, cssReaderTree], {
         overwrite: true
       });
     }
